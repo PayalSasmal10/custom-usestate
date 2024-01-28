35e5eca5eb424fd3247569360516d0f26b817bdb
Payalsasmal10 <sasmalpayal@gmail.com>
2024-01-28 19:48:25 +0530

    increaing and decreasing is working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
+const useCustState = (defaultValue) => {
+  const setDefaultValue = (newValue) => {
+    // do something
+    defaultValue = newValue;
+    console.log(defaultValue);
+
+  };
+  console.log("outside", defaultValue);
+  const state = [defaultValue, setDefaultValue];
+  return state;
+}
+
 function App() {
+  const [count, setCounter] = useCustState(4);
+
+  const onIncrementButtonHandler = () =>{
+    setCounter(count+1);
+  }
+
+  const onDecrementButtonHandler = () => {
+    setCounter(count-1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="button" onClick={onDecrementButtonHandler}>-</button>
+      <span>{count}</span>
+      <button className="button" onClick={onIncrementButtonHandler}>+</button>
     </div>
   );
 }
